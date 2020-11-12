09ab213c06f44510187bd81aad81c7c8e5463866
Sebastien Duthil <sduthil@wazo.community>
2020-11-12 14:22:42 -0500

    rename to wazo-cel-plugin
    Also rename Python package

--- setup.py
+++ setup.py
@@ -6,23 +6,20 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='wazo_cel',
+    name='wazo-cel',
     version='1.0.0',
     description='Wazo CEL API',
-
     author='Wazo Authors',
     author_email='dev@wazo.community',
-
     url='http://wazo-platform.org',
-
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'wazo_cel': ['*/api.yml'],
+        'wazo_call_logd_wazo_cel_official': ['api.yml'],
     },
     entry_points={
         'wazo_call_logd.plugins': [
-            'cel = wazo_cel.call_logd.plugin:Plugin',
+            'cel = wazo_call_logd_wazo_cel_official.plugin:Plugin',
         ]
-    }
+    },
 )

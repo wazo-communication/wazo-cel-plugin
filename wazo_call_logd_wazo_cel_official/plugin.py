059dd446745f33db174b2b6deac2244ea6c1a78d
Sebastien Duthil <sduthil@wazo.community>
2021-08-02 11:18:22 -0400

    add tenant filter

--- wazo_call_logd_wazo_cel_official/plugin.py
+++ wazo_call_logd_wazo_cel_official/plugin.py
@@ -6,6 +6,7 @@ import logging
 from wazo_auth_client import Client as AuthClient
 from wazo_call_logd.database.helpers import new_db_session
 from wazo_call_logd.database.queries.base import BaseDAO
+from wazo_call_logd.database.models import CallLog as CallLogSchema
 from xivo_dao.alchemy.cel import CEL as CELSchema
 from xivo.tenant_helpers import UnauthorizedTenant
 
@@ -48,8 +49,9 @@ class CELService:
                 'Service tenant is not initialized, rejecting request as Unauthorized'
             )
             raise UnauthorizedTenant(search_params['tenant_uuid'])
-        if self._service_tenant_uuid != search_params['tenant_uuid']:
-            raise UnauthorizedTenant(search_params['tenant_uuid'])
+        if self._service_tenant_uuid == search_params['tenant_uuid']:
+            # master tenant, remove tenant filter
+            search_params.pop('tenant_uuid', None)
 
         cel = self._cel_dao.find_all(search_params)
         return cel
@@ -71,6 +73,10 @@ class CELDAO(BaseDAO):
                 query = query.filter(CELSchema.uniqueid == params['uniqueid'])
             if params.get('call_log_id'):
                 query = query.filter(CELSchema.call_log_id == params['call_log_id'])
+            if params.get('tenant_uuid'):
+                query = query.join(
+                    CallLogSchema, CallLogSchema.id == CELSchema.call_log_id
+                ).filter(CallLogSchema.tenant_uuid == params['tenant_uuid'])
             if params.get('limit'):
                 query = query.limit(params['limit'])
 
